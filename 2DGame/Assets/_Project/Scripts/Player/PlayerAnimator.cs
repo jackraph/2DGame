ccff92f433f9617f0c92caad4476d628c486479a
jackraph <s3884794@student.rmit.edu.au>
2021-10-08 12:37:03 +1100

    New tileset

--- 2DGame/Assets/_Project/Scripts/Player/PlayerAnimator.cs
+++ 2DGame/Assets/_Project/Scripts/Player/PlayerAnimator.cs
@@ -10,24 +10,41 @@ public class PlayerAnimator : MonoBehaviour
 
     private void Awake()
     {
-        _anim = GetComponent<Animator>();
+        //_anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        _anim.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
-        _anim.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
-
+        // _anim.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+        // _anim.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+        //
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
+        //
+        // if (x != 0 && y != 0)
+        // {
+        //     _anim.SetBool("Diagonal", true);  
+        // }
+        // else
+        // {
+        //     _anim.SetBool("Diagonal", false);
+        // }
+        
+        
 
-        if (x != 0 && y != 0)
+        //Flip sprite based on direction.
+        if (x > 0)
         {
-            _anim.SetBool("Diagonal", true);  
-        }
-        else
+            transform.localScale = Vector3.one;
+        } 
+        else if (x < 0)
         {
-            _anim.SetBool("Diagonal", false);
+            transform.localScale = new Vector3(-1, 1, 1);
         }
+        //~~~~~~~~~~~~~~~~~~~
     }
+    
+    
+    
+    
 }
